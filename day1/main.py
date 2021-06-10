738b7bca79a37127d3f31af9bbf1f44ffa168038
CW-MD <71305304+CW-MD@users.noreply.github.com>
2021-06-09 22:25:44 -0500

    Delete day1 directory

--- day1/main.py
+++
@@ -1,9 +0,0 @@
-#
-print("Band Name Generator")
-
-user_adj = input("Use one word to describe yourself: ")
-user_pet = str(len(input("What is the name of your pet? "))) 
-#user_name= str(len(user_pet))
-
-
-print("Your band name is: The " + user_adj + " " + user_pet )
