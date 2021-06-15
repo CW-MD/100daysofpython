773acef6d61a9b2c1e224571e0e887ce3bdb8a5d
Chad <cdayton96@gmail.com>
2021-06-14 23:24:19 -0500

    feat: looped gameplay, added win/loss checks

--- week1/day 7/main.py
+++ week1/day 7/main.py
@@ -5,22 +5,39 @@ word_list=['cat', 'monkey', 'pneumonoultramicroscopicsilicovolcanoconiosis']
 
 chosen_word = random.choice(word_list)
 
-guess = input('please guess a letter').lower()
+print(f'word is {chosen_word}')
+
+display = []
+
+for letter in chosen_word:
+     display.append('_')
+print(display)
 
 previous_guesses = []
 
 lives = 6
 
-if guess not in previous_guesses:
-    previous_guesses.append(guess)
-    if guess in chosen_word:
-        for letter in chosen_word:
-            if letter == guess:
-                print(guess)
-            else:
-                print('?')
+win = False
+
+while win != True:
+    guess = input('please guess a letter').lower()
+    if guess not in previous_guesses:
+        previous_guesses.append(guess)
+        if guess in chosen_word:
+            for x in range(len(display)):
+                if chosen_word[x] ==guess :
+                    display[x] = guess
+                    print(display)
+                    if '_' not in display:
+                        win=True
+        else:
+            lives-=1
+            print(f'word does not contain {guess}; {lives} lives remain')
+            if lives < 1:
+                print('You Lose')
+                break
     else:
-        lives-=1
-        print(f'word does not contain {guess}; {lives} lives remain')
-else:
-    print(f'{guess} was already guessed')
+        print(f'{guess} was already guessed')
+if win == True:
+    print(f'You Win! The word was: {chosen_word}')
+    
