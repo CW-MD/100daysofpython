7c1d6d97d87d6ee6c3c78038c61fd21452f20b8a
Chad <cdayton96@gmail.com>
2021-06-16 22:24:41 -0500

    created dictionary to hold user : bid data

--- week2/day9/main.py
+++ week2/day9/main.py
@@ -2,19 +2,26 @@
 #While 'More Bidders' != N 
 import art
 
+cur_data = {}
+
+
 def auction():
     more_bids = True
+    
     print(art.image)
 
     while more_bids != False:
         name = input('What is your name?')
         bid = input('What is your bid?')
+        listMaker(name, bid)
         bidders = input('Are there more bidders? "Y" / "N" ?').upper()
         if bidders == 'N':
+            print(cur_data)
             more_bids = False
 
 
-
+def listMaker(name, bid):
+    cur_data[name] = bid
 
 
 auction()
