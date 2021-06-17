051a4d78b92be332773f73959d7436359c9e1b8b
Chad <cdayton96@gmail.com>
2021-06-17 17:12:16 -0500

    feat: updated maxFinder, refactored

--- week2/day9/main.py
+++ week2/day9/main.py
@@ -7,21 +7,30 @@ cur_data = {}
 
 def auction():
     more_bids = True
-    
     print(art.image)
 
     while more_bids != False:
         name = input('What is your name?')
-        bid = input('What is your bid?')
+        bid = int(input('What is your bid? $'))
         listMaker(name, bid)
         bidders = input('Are there more bidders? "Y" / "N" ?').upper()
         if bidders == 'N':
             print(cur_data)
+            maxFinder(cur_data)
             more_bids = False
 
 
 def listMaker(name, bid):
     cur_data[name] = bid
 
+
+def maxFinder(dict):
+    highest_bid = 0
+    winner = ''
+    for key in dict:
+        if dict[key] > highest_bid:
+            highest_bid = dict[key]
+            winner = key
+    print(f'The winner is{winner}, with a bid of ${highest_bid}!')
 
 auction()
