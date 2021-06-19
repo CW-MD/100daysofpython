92d829b8252d4f726fcacbe008374125053049e1
Chad <cdayton96@gmail.com>
2021-06-19 14:21:39 -0500

    refactors

--- week2/day10/main.py
+++ week2/day10/main.py
@@ -23,27 +23,28 @@ ops = {
     }
 
 symbols = '+ - * /'
-should_continue = True
+def calculator():
+    should_continue = True
 
-num1 = int(input('What is your first number? :'))
-while(should_continue):
-    print(f"Operations: {symbols}")
-    cur_op = input('Please pick an operation: ')
+    num1 = float(input('What is your first number? :'))
+    while(should_continue):
+        print(f"Operations: {symbols}")
+        cur_op = input('Please pick an operation: ')
 
-    num2 = int(input('What is your next number? :'))
-
-
-    calc_function = ops[cur_op]
-    answer = calc_function(num1, num2)
-    print(answer)
-
-    repeat = input('Would you like to perform another operation?: Y / N : ').lower()
-    if repeat == 'y':
-        num1 = answer
-    if repeat == 'n':
-        should_continue = False
+        num2 = float(input('What is your next number? :'))
 
 
+        calc_function = ops[cur_op]
+        answer = calc_function(num1, num2)
+        print(answer)
+
+        repeat = input('Would you like to perform another operation?: Y / N : ').lower()
+        if repeat == 'y':
+            num1 = answer
+        if repeat == 'n':
+            should_continue = False
+            calculator()
+calculator()
 
 #MFW my current version doesnt support match-case :(
 # match cur_op:
