9b8d396e004f1b65955a232c100c2383f7557e9a
Chad <cdayton96@gmail.com>
2021-06-25 15:04:40 -0500

    feat: funcs for change,and  resource management -- project fully functional

--- week3/day15/main.py
+++ week3/day15/main.py
@@ -1,12 +1,7 @@
 #Coffee Machine
 import data
-#establishing common inputs as variables to avoid typos
-espresso = 'espresso'
-latte = 'latte'
-cappuccino = 'cappuccino'
-off = 'off'
-report = 'report'
-cents = 0
+#
+machine_money = 0
 
 
 def user_input():
@@ -22,6 +17,7 @@ def user_input():
 
 
 def check_resources(drink_name):
+    global machine_money
     drink_ingredients = data.MENU[drink_name]['ingredients']
     for ingred in drink_ingredients:
         #print(ingred)
@@ -32,12 +28,18 @@ def check_resources(drink_name):
             break
     else:
         print('Please insert coins uwu')
-        take_money()
+        money_input = take_money()
+        if money_input >= data.MENU[drink_name]['cost']:
+            machine_money += issue_change(money_input, drink_name)
+            deduct_resources(drink_name)
+            print(f'Here is your {drink_name}')
+        else:
+            print('need more money')
             
 
 def take_money():
     monies = []
-    quarters = int(input('Quarters: '))*25
+    quarters =int(input('Quarters: '))*25
     dimes = int(input('Dimes: '))*10
     nickels = int(input('Nickels: '))*5
     pennies = int(input('Pennies: '))
@@ -45,7 +47,7 @@ def take_money():
     monies.append(dimes)
     monies.append(nickels)
     monies.append(pennies)
-    return sum(monies)
+    return (sum(monies) / 100)
 
 def print_report():
     values = data.resources
@@ -53,5 +55,16 @@ def print_report():
         print(f'{key} : {values[key]} ')
     
 
-#print_report()
+def issue_change(money, drink_name):
+    cost = float(data.MENU[drink_name]['cost'])
+    change = (money - cost)
+    print(f'Your change is ${round(change,5)}')
+    return cost
+    
+def deduct_resources(drink_name):
+    drink_ingredients = data.MENU[drink_name]['ingredients']
+    for ingred in drink_ingredients:
+        data.resources[ingred] -= drink_ingredients[ingred]
+        
+        
 user_input()
--- week3/day15/test_sample.py
+++ week3/day15/test_sample.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest, data
 def take_money(q=0,d=0,n=0,s=0):
     monies = []
     q *= 25
@@ -10,7 +10,14 @@ def take_money(q=0,d=0,n=0,s=0):
     monies.append(s)
     return sum(monies)
 
+def issue_change(money, drink_name):
+    cost = float(data.MENU[drink_name]['cost'])
+    return money - cost
+
 def test_answer():
     assert take_money(1,1,1,1) == 41 
     assert take_money() == 0
     assert take_money(0,1,0,1) == 11
+    assert issue_change(2.5,'latte') == 0
+    assert issue_change(3,'espresso') == 1.5
+    assert issue_change(65,'cappuccino') == 62
