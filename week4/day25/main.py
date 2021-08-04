587bd0b6ccf1d7201c41c70e745127cbde0c1d1d
CW-MD <cdayton96@gmail.com>
2021-08-03 23:33:54 -0400

    day 25: Pandas-Play

--- week4/day25/main.py
+++ week4/day25/main.py
@@ -30,3 +30,22 @@ monday = (data[data.day == 'Monday'])
 #print(monday.to)
 
 print(convert_to_farenheit(monday.temp))
+
+squirrel_data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+
+#print(squirrel_data[squirrel_data['Primary Fur Color']] == 'Gray')
+gray_squirrels = len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Gray'])
+black_squirrels = len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Black'])
+cinnamon_squirrels = len(squirrel_data[squirrel_data['Primary Fur Color'] == 'Cinnamon'])
+print(gray_squirrels)
+print(black_squirrels)
+print(cinnamon_squirrels)
+
+sq_dict = {
+
+   "Fur Color":['Gray', 'Cinnamon', 'Black'] ,"Count":[gray_squirrels, cinnamon_squirrels, black_squirrels]
+}
+
+dFrame = pandas.DataFrame(sq_dict)
+dFrame.to_csv("squirrel_count.csv")
+
