07cc1e0baba9738ea718375d59517473776f4602
CW-MD <cdayton96@gmail.com>
2021-08-05 22:34:44 -0400

    feat: working game loop on states game

--- week4/day26/main.py
+++ week4/day26/main.py
@@ -1,28 +1,31 @@
 import turtle
 import pandas
 screen = turtle.Screen()
-screen.setup(width=720, height=500)
+#screen.setup(width=720, height=500)
 screen.title('50 States Guessing Game')
-image = 'blank_states_img.gif'
+image = 'day26/blank_states_img.gif'
 screen.addshape(image)
 
 turtle.shape(image)
 game_running = True
 guessed_states = []
-state_list = pandas.read_csv('50_states.csv')
+state_raw = pandas.read_csv('day26/50_states.csv')
+state_list = state_raw.state.to_list()
 
-user_input = screen.textinput('Guess a State', "Name another state")
 
-print(state_list[state_list.state == user_input])
+while len(guessed_states) < 50:
+    user_input = screen.textinput(f'{len(guessed_states)}/50', "Name another state").title()
 
-while game_running:
-    user_input = screen.textinput('Guess a State', "Name another state")
-    if state_list[state_list.state == user_input]:
-        guessed_states.append(state_list[state_list.state])
-        turtle.pu()
-        turtle.goto(state_list[state_list.state == user_input].x, state_list[state_list.state == user_input].y)
-        turtle.pd()
-        turtle.write(state_list[state_list.state == user_input].state)
+    if user_input in state_list:
+        cur_state = state_raw[state_raw.state == user_input]
+        print(cur_state)
+        writer = turtle.Turtle()
+        writer.hideturtle()
+        writer.pu()
+        guessed_states.append(user_input)
+        writer.goto(int(state_raw[state_raw.state == user_input].x), int(state_raw[state_raw.state == user_input].y))
+        writer.pd()
+        writer.write(user_input)
 
 turtle.mainloop()
 
